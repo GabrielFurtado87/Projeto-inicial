8d543b0a0b7da6448f803bf5097312bb5a2614dd
Gabriel Furtado <gabrielprog87@gmail.com>
2023-10-23 19:38:29 -0300

    Cadastro de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,7 +12,10 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProdutosDAO {
@@ -23,9 +26,29 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        listagem.add(produto);
         conn = new conectaDAO().connectDB();
+        
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (NOME,VALOR,ESTATUS) VALUES (?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso");
+            
+        } catch (SQLException sQLException) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar " + sQLException);
+        }finally{
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ProdutosDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+        
+        
+        
         
         
     }
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -2,6 +2,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
@@ -17,17 +19,30 @@ import javax.swing.JOptionPane;
  */
 public class conectaDAO {
     
+    private final String Driver = "com.mysql.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost/leiloes";
+    private final String User = "root";
+    private final String Pass = "";
+    Connection conn = null;
     public Connection connectDB(){
-        Connection conn = null;
-        
+               
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection(URL,User,Pass);
+            System.out.println("Conectado!!");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
+    
+   //public void close(){
+    //    try {
+    //        conn.close();
+    //    } catch (SQLException ex) {
+    //        Logger.getLogger(conectaDAO.class.getName()).log(Level.SEVERE, null, ex);
+    //    }
+   //}
     
 }
